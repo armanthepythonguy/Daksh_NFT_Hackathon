a28096053b171854016f1a44ec24567eea8f8ded
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2022-06-18 11:25:55 +0530

    Front Page

---
+++ food_frontend/src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Made By Abdul, Arman, Chidam, Subramaniyan</div>
+  );
+};
+
+export default Footer;
